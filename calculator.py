6a7f3f60175ac20621db943a0ece0b5ce4e991b8
Christian David <cris920sdm@hotmail.com>
2022-11-29 16:23:28 -0500

    Git tuto

---
+++ calculator.py
@@ -0,0 +1,22 @@
+import operations
+import sys
+
+operation=sys.argv[1]
+num1=int(sys.argv[2])
+num2=int(sys.argv[3])
+
+def main():
+    if operation == 'sumar':
+        print(operations.add(num1,num2))
+    elif operation == 'resta':
+        print(operations.sustract(num1,num2))
+    elif operation == 'multiplicar':
+        print(operations.multiply(num1,num2))
+    elif operation == 'dividir':
+        print(operations.divide(num1,num2))
+    else:
+        print('Operacion no existe.')
+
+if __name__ == '__main__':
+    main()
+
